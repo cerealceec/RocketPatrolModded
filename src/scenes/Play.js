66c76b7f5c90e3ab8465e8f2a2a3d04c9eaa4d37
Cecil Choi <ceec@ucsc.edu>
2022-04-29 19:05:32 -0700

    deleted comment

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -36,13 +36,6 @@ class Play extends Phaser.Scene {
 
         // borders
         this.borders = this.add.image(0, 0, 'borders').setDepth(3).setOrigin(0, 0);
-        /*
-        // white borders
-        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0).setDepth(3);
-        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0).setDepth(3);
-        this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0).setDepth(3);
-        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0).setDepth(3);
-        */
 
         // add rocket (p1)
         this.p1Rocket = new Rocket(this, game.config.width / 2, game.config.height - borderUISize - borderPadding, 'rocket', 0, 1).setOrigin(0.5, 0);
